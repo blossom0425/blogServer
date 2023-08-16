9bcbc6a60507ba851bacdb9773999bf7eac49406
blossom <1124094082@qq.com>
2023-08-16 18:39:06 +0800

    进度条+动态标题

---
+++ assets/homeNote-40acff80.js
@@ -0,0 +1,34 @@
+import { l as leftNav } from "./leftNav-718896cf.js";
+import { r as rightNav } from "./rightNav-d36f9cb9.js";
+import { _ as _export_sfc, r as ref, q as resolveComponent, a as openBlock, c as createElementBlock, h as createVNode, g as unref, s as isRef, d as createBaseVNode } from "./index-de877cc2.js";
+const homeNote_vue_vue_type_style_index_0_scoped_a63f8b4c_lang = "";
+const _hoisted_1 = {
+  class: "homeNote"
+};
+const _hoisted_2 = {
+  class: "mainArticle"
+};
+const _sfc_main = {
+  __name: "homeNote",
+  setup(__props) {
+    const data_left_nav = ["HTML基础", "CSS基础", "CSS进阶", "JS基础", "JS DOM操作", "Node.js", "Promise", "Vue2", "Vue3", "Vue2和Vue3的对比", "测试一个特别特别特别特别特别特别特别长的标题别特别特别特别特别特别长的标题别特别特别特别特别特别长的标题", "Node.js", "Promise", "Vue2", "Vue3", "Vue2和Vue3的对比", "测试一个特别特别特别特别特别特别特别长的标题", "Node.js", "Promise", "Vue2", "Vue3", "Vue2和Vue3的对比", "测试一个特别特别特别特别特别特别特别长的标题"];
+    let active_nav = ref("");
+    return (_ctx, _cache) => {
+      const _component_router_view = resolveComponent("router-view");
+      return openBlock(), createElementBlock("div", _hoisted_1, [createVNode(leftNav, {
+        ref: "left-nav",
+        activeTitle: unref(active_nav),
+        "onUpdate:activeTitle": _cache[0] || (_cache[0] = ($event) => isRef(active_nav) ? active_nav.value = $event : active_nav = $event),
+        title: "学习笔记",
+        nav_data: data_left_nav
+      }, null, 8, ["activeTitle"]), createBaseVNode("div", _hoisted_2, [createVNode(_component_router_view)]), createVNode(rightNav, {
+        title: unref(active_nav),
+        nav_data: data_left_nav
+      }, null, 8, ["title"])]);
+    };
+  }
+};
+const homeNote = /* @__PURE__ */ _export_sfc(_sfc_main, [["__scopeId", "data-v-a63f8b4c"]]);
+export {
+  homeNote as default
+};
---
+++ assets/timeLine-7dbe9c6c.js
@@ -0,0 +1,74 @@
+import { r as rightSum } from "./rightSum-2a829be0.js";
+import { _ as _export_sfc, i as inject, r as ref, o as onMounted, a as openBlock, c as createElementBlock, d as createBaseVNode, F as Fragment, b as renderList, g as unref, h as createVNode, y as request, t as toDisplayString } from "./index-ae3c142f.js";
+const timeLine_vue_vue_type_style_index_0_scoped_9a943aea_lang = "";
+const _hoisted_1 = {
+  class: "timeLine"
+};
+const _hoisted_2 = {
+  class: "timeBlocks mycard"
+};
+const _hoisted_3 = {
+  class: "timeTitle"
+};
+const _hoisted_4 = {
+  class: "record"
+};
+const _hoisted_5 = ["onClick"];
+const _hoisted_6 = {
+  class: "record_item_time",
+  style: {
+    "margin-right": "10px"
+  }
+};
+const _hoisted_7 = {
+  class: "record_item_title"
+};
+const _sfc_main = {
+  __name: "timeLine",
+  setup(__props) {
+    const api = inject("api");
+    let timeBlockArr = /* @__PURE__ */ new Map();
+    let blogsArr = ref([]);
+    const initData = function() {
+      request({
+        url: api.blog.timeline
+      }).then((res) => {
+        let temp = res.blogs;
+        for (let blog of temp) {
+          let y_m = blog.create_time.slice(0, 7);
+          if (!timeBlockArr.get(y_m))
+            timeBlockArr.set(y_m, []);
+          timeBlockArr.get(y_m).push(blog);
+        }
+        for (let key of timeBlockArr.keys())
+          blogsArr.value.push([key, timeBlockArr.get(key)]);
+      }).catch((err) => {
+        console.log(err);
+      });
+    };
+    onMounted(() => {
+      initData();
+    });
+    const checkDetail = function(id) {
+      window.open("#/detail?id=" + id);
+    };
+    return (_ctx, _cache) => {
+      return openBlock(), createElementBlock("div", _hoisted_1, [createBaseVNode("div", _hoisted_2, [(openBlock(true), createElementBlock(Fragment, null, renderList(unref(blogsArr), (blogs) => {
+        return openBlock(), createElementBlock("div", {
+          class: "timeBlock",
+          key: blogs[0]
+        }, [createBaseVNode("h2", _hoisted_3, toDisplayString(blogs[0]), 1), createBaseVNode("ul", _hoisted_4, [(openBlock(true), createElementBlock(Fragment, null, renderList(blogs[1], (blog) => {
+          return openBlock(), createElementBlock("li", {
+            class: "record_item",
+            key: blog.id,
+            onClick: ($event) => checkDetail(blog.id)
+          }, [createBaseVNode("span", _hoisted_6, toDisplayString(blog.create_time.slice(0, 10)), 1), createBaseVNode("span", _hoisted_7, toDisplayString(blog.title), 1)], 8, _hoisted_5);
+        }), 128))])]);
+      }), 128))]), createVNode(rightSum)]);
+    };
+  }
+};
+const timeLine = /* @__PURE__ */ _export_sfc(_sfc_main, [["__scopeId", "data-v-9a943aea"]]);
+export {
+  timeLine as default
+};
